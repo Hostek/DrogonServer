cedaa52d2e2df83d5fdb912f188c92d72db8bccb
Hostek <michalpl245674477@gmail.com>
2022-08-26 16:47:11 +0200

    Basic setup

---
+++ DrogonServer/DrogonServer.h
@@ -0,0 +1,10 @@
+﻿// DrogonServer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include <drogon/drogon.h>
+
+// TODO: Reference additional headers your program requires here.
