497a422008aa6b8fe075ce8fffe52c6ad1492772
Hostek <michalpl245674477@gmail.com>
2022-08-27 16:20:27 +0200

    Changed code structure

--- DrogonServer/DrogonServer.cpp
+++ DrogonServer/DrogonServer.cpp
@@ -3,53 +3,8 @@
 int main() {
     using namespace drogon;
 
-    app().registerHandler(
-        "/",
-        [](const HttpRequestPtr&,
-            std::function<void(const HttpResponsePtr&)>&& callback) {
-                auto resp = HttpResponse::newHttpResponse();
-                resp->setBody("Hello, World!");
-                callback(resp);
-        },
-        { Get });
+    std::cout << "Server is running on 127.0.0.1:8848" << std::endl;
 
-    app().registerHandler(
-        "/user/{user-name}",
-        [](const HttpRequestPtr&,
-            std::function<void(const HttpResponsePtr&)>&& callback,
-            const std::string& name) {
-                auto resp = HttpResponse::newHttpResponse();
-                resp->setBody("Hello, " + name + "!");
-                callback(resp);
-        },
-        { Get });
-
-    app().registerHandler(
-        "/hello?user={user-name}",
-        [](const HttpRequestPtr&,
-            std::function<void(const HttpResponsePtr&)>&& callback,
-            const std::string& name) {
-                auto resp = HttpResponse::newHttpResponse();
-                resp->setBody("Hello, " + name + "!");
-                callback(resp);
-        },
-        { Get });
-
-    app().registerHandler(
-        "/hello_user",
-        [](const HttpRequestPtr& req,
-            std::function<void(const HttpResponsePtr&)>&& callback) {
-                auto resp = HttpResponse::newHttpResponse();
-                auto name = req->getOptionalParameter<std::string>("user");
-                if (!name)
-                    resp->setBody("Please tell me your name");
-                else
-                    resp->setBody("Hello, " + name.value() + "!");
-                callback(resp);
-        },
-        { Get });
-
-    LOG_INFO << "Server running on 127.0.0.1:8848";
     app().addListener("127.0.0.1", 8848).run();
 
 	return 0;
---
+++ DrogonServer/pages/Home/HomePageController.cpp
@@ -0,0 +1,21 @@
+#include <drogon/HttpSimpleController.h>
+
+class HomePageController : public drogon::HttpSimpleController<HomePageController> {
+public:
+    PATH_LIST_BEGIN
+        PATH_ADD("/");
+    PATH_LIST_END
+
+    void asyncHandleHttpRequest(
+         const drogon::HttpRequestPtr& req,
+         std::function<void(const drogon::HttpResponsePtr&)>&& callback
+    ) override {
+        auto res = drogon::HttpResponse::newHttpResponse();
+        res->setBody(
+            "Hello, this is a generic hello message from the HomePage "
+            "controller");
+        callback(res);
+    }
+
+    
+};
